2ce9d22664491374d765381090c4bc02f40e59cb
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 20:34:50 +0000

    Automatically backed up by Learn

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -7,7 +7,7 @@ class VisitorsController < ApplicationController
   end
   def signout
     session.clear
-    flash[:alert] = "Access denied."
+    flash[:alert] = "Signed out successfully."
     redirect_to root_path
   end
 end
