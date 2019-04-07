32b068844df0ab76cb551a3b751aab14ab6e4688
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 20:20:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
       redirect_to '/home'
     end
     @post = Post.find_by(id: params[:id])
-    if !current_user.admin? && @post.user_id != session[:user_id]
+    if !current_user.admin? && !current_user.vip? && @post.user_id != session[:user_id]
       redirect_to '/home'
     else
       @post.update(post_params)
