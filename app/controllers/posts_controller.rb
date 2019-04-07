a9e39dcc278b0adbf90b174dfc723bd83987bad5
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 20:03:52 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find_by(id: params[:id])
   end
   def index
+    @posts = Post.all
   end
   def edit
+    @post = Post.find_by(id: params[:id])
   end
   def update
   end
