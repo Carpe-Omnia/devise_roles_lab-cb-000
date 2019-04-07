5faabdfe13316f61e22dbc23b1d3f6067e736e33
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-07 20:15:33 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find_by(id: params[:id])
+    if !@user 
+      flash[:alert] = "Access denied."
+      redirect_to root_path
+    end   
     if !current_user.admin? && current_user != @user
       flash[:alert] = "Access denied."
       redirect_to root_path
